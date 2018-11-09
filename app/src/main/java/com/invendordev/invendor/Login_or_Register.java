04f5a538fef5a9d48a172ed7de7b15649d159f6d
rockround <zachishimaru@gmail.com>
2018-11-09 17:25:19 -0600

    Employed Amazon
    Made login system entirely amazon-based, added custom ui, got databases working.

--- app/src/main/java/com/invendordev/invendor/Login_or_Register.java
+++ app/src/main/java/com/invendordev/invendor/Login_or_Register.java
@@ -2,6 +2,7 @@ package com.invendordev.invendor;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
@@ -9,48 +10,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
+import com.amazonaws.mobile.auth.ui.SignInUI;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
 public class Login_or_Register extends AppCompatActivity {
 
-    private Button btnNavLogin;
-    private Button btnNavRegister;
-    private Button rootButton;
     Context context = this;
 
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_or_register);
-
-        btnNavLogin = (Button) findViewById(R.id.btnNavLogin);
-        btnNavRegister = (Button) findViewById(R.id.btnNavRegister);
-        rootButton = (Button) findViewById(R.id.rootButton);
-
-        btnNavRegister.setOnClickListener(new View.OnClickListener() {
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(context, "Clicked Register button", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(context, RegisterActivity.class);
-                startActivity(intent);
+            public void onComplete(final AWSStartupResult awsStartupResult) {
+                AuthUIConfiguration config =
+                        new AuthUIConfiguration.Builder()
+                                .userPools(true)  // true? show the Email and Password UI
+                                .logoResId(R.mipmap.ic_launcher_foreground) // Change the logo
+                                .backgroundColor(Color.WHITE) // Change the backgroundColor
+                                .isBackgroundColorFullScreen(true) // Full screen backgroundColor the backgroundColor full screen
+                                .fontFamily("sans-serif-light") // Apply sans-serif-light as the global font
+                                .canCancel(true)
+                                .build();
+                SignInUI signinUI = (SignInUI) AWSMobileClient.getInstance().getClient(Login_or_Register.this, SignInUI.class);
+                signinUI.login(Login_or_Register.this, SwipeScreen.class).authUIConfiguration(config).execute();
             }
-        });
-
-        btnNavLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(context, "Clicked Login button", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(context, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        rootButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(context, "You've bypassed the login!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(context, SwipeScreen.class);
-                startActivity(intent);
-            }
-        });
-
+        }).execute();
     }
 }
--- app/src/main/java/com/invendordev/invendor/SwipeScreen.java
+++ app/src/main/java/com/invendordev/invendor/SwipeScreen.java
@@ -32,7 +32,6 @@ public class SwipeScreen extends AppCompatActivity{
     DynamoDBMapper dynamoDBMapper;
     private int timesDown = 0;
     private int timesUp = 0;
-    private Socket socket;
     private String name;
     PrintWriter out;
     SwipeScreen(){
@@ -48,48 +47,10 @@ public class SwipeScreen extends AppCompatActivity{
     }
 
     @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideSystemUI();
-        }
-    }
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
-
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
-            @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
-                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(
-                        SwipeScreen.this,
-                        SignInUI.class);
-                signin.login(
-                        SwipeScreen.this,
-                        SwipeScreen.class).execute();
-            }
-        }).execute();
+
 
         AWSCredentialsProvider credentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
         AWSConfiguration configuration = AWSMobileClient.getInstance().getConfiguration();
@@ -102,9 +63,8 @@ public class SwipeScreen extends AppCompatActivity{
                 .dynamoDBClient(dynamoDBClient)
                 .awsConfiguration(configuration)
                 .build();
-        createNews(credentialsProvider.getCredentials().getAWSAccessKeyId());
         setContentView(R.layout.activity_swipe_screen);
-
+        loadProject("Rocket");
 
         final View background  = findViewById(R.id.background);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -130,7 +90,6 @@ public class SwipeScreen extends AppCompatActivity{
                         Log.i("Samuel", "Swiped down: " + timesDown);
                     }
                 }
-                new Thread(new ClientThread()).start();
 
                 return true;
             }
@@ -176,62 +135,18 @@ public class SwipeScreen extends AppCompatActivity{
     public void setTimesDown(int i){
         timesDown = i;
     }
-    class ClientThread implements Runnable{
-        private static final int SERVERPORT = 1;
-        private static final String SERVER_IP = "10.34.7.38";
 
-        @Override
-        public void run() {
-            try {
-                //InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-
-                socket = new Socket("10.34.16.154", SERVERPORT);
-
-                Log.i("pswd", "in cllientthread");
-                try {
-
-                    out = new PrintWriter(new BufferedWriter(
-                            new OutputStreamWriter(socket.getOutputStream())),
-                            true);
-                    out.println("Verify AkitoIto dog");
-                    out.println("COUNT " + timesUp + " " + timesDown);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-
-        }
-    }
-    public void createNews(String uid) {
-        final ProjectDataDO newsItem = new ProjectDataDO();
-
-        newsItem.setUserId(uid);
-        newsItem.setYoutubeData("This is the article content");
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                dynamoDBMapper.save(newsItem);
-                // Item saved
-            }
-        }).start();
-    }
-    public void readNews(final String uid) {
+    public void loadProject(final String uid) {
         new Thread(new Runnable() {
             @Override
             public void run() {
 
-                ProjectDataDO newsItem = dynamoDBMapper.load(
+                ProjectDataDO projectItem = dynamoDBMapper.load(
                         ProjectDataDO.class,
                         uid);
 
                 // Item read
-                // Log.d("News Item:", newsItem.toString());
+                 Log.d("ProjectDetails", projectItem.getYoutubeData());
             }
         }).start();
     }
